54d19b062fa3366c749c82cf31657ac250af2d68
Sam Bunn <sammbunnn@gmail.com>
2023-09-14 16:23:54 -0500

    attempted fucntions

--- src/queries.js
+++ src/queries.js
@@ -20,16 +20,31 @@ export const query5 = await Human.findAll({where: {fname: {[Op.startsWith]: 'J'}
 export const query6 = await Animal.findAll({where: {birthYear: {[Op.is]: null}}}) ;
 
 // Get all the animals with species "fish" OR "rabbit"
-export const query7 = null;
+export const query7 = await Animal.findAll({where: {[Op.or]: [{species: 'fish'},{species: 'rabbit'}]}});
 
 // Get all the humans who DON'T have an email address that contains "gmail"
-export const query8 = null;
+export const query8 = await Human.findAll({where: {email: {[Op.notLike]: '%gmail.com'}}});
 
 // Continue reading the instructions before you move on!
 
 // Print a directory of humans and their animals
-export async function printHumansAndAnimals() {}
+export async function printHumansAndAnimals() {
+  let humans = await Human.findAll()
+    for(let i = 0; i < humans.length; i++){
+      // console.log(humans)
+      let animals = await Animal.findAll({where: {humanId: [humans[i].humanId]}})
+     } for (let i = 0; i < animals.length; i++){
+      console.log(animals)
+     } 
+}
+console.log(printHumansAndAnimals())
+
 
 // Return a Set containing the full names of all humans
 // with animals of the given species.
-export async function getHumansByAnimalSpecies(species) {}
+export async function getHumansByAnimalSpecies(species) {
+  const humanAnimalSet = new Set()
+  const idFinder = await Animal.findAll({where: { species: {Animal.species}}})
+  humanAnimalSet.add(humans)
+}
+console.log(getHumansByAnimalSpecies())
